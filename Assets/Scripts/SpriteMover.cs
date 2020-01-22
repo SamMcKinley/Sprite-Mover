677799e67cb675920d61e4c3020acda706837f51
Sam McKinley <sammckin@uat.edu>
2020-01-22 12:25:31 -0700

    Sprite Mover

---
+++ Assets/Scripts/SpriteMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour
+{
+    public Transform tf;
+
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move up if the player presses up
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            //Only Works if shift is held down
+        }
+        else
+        {
+            //Only Works if shift is not held down
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            tf.position = tf.position + Vector3.up * Time.deltaTime * speed;
+        }
+        //This works if player is holding down shift or if player is not holding down shift
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+        tf.position = new Vector3(0,0,0);
+        }
+        
+    }
+    
+        
+}
